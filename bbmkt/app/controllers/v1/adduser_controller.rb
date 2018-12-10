1f87b06e42c2fb67f2c890dbb873053f9bfa94c2
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2018-12-09 19:11:27 -0600

    cors

--- bbmkt/app/controllers/v1/adduser_controller.rb
+++ bbmkt/app/controllers/v1/adduser_controller.rb
@@ -3,4 +3,22 @@ class V1::AdduserController < ApplicationController
 		@adduser = User.all
 		render json: @adduser, status: :ok
 	end
+	def create
+		@adduser = User.new(adduser_params)
+		@adduser.save
+		render json: @adduser, status: :created
+	end		
+	def delete
+		@adduser = User.where(id_user: params[:id_user]).first
+		
+		if  @adduser.destroy
+			head[:ok]
+		else
+			head[:unprocesable_entity]
+		end
+	end		
+	private
+	def adduser_params
+		params.require(:adduser).permit(:id_user, :first_name, :laste_name, :email, :mobilephone, :alias, :pass, :descrip, :street, :home_number, :colony, :city, :state, :country, :postal_code)
+	end
 end
